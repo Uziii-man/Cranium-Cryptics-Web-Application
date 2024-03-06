f0b7b61f8f7377daa98e0e300cd5f1a30606dbed
Lakshan Cooray <Lakshancooray23@gmail.com>
2024-03-07 02:03:02 +0530

    connected the backend with pdf report generation

--- Web/app.py
+++ Web/app.py
@@ -84,11 +84,6 @@ def account():
     return render_template('account.html')
 
 
-@app.route('/report')
-def report():
-    return render_template('report.html')
-
-
 @app.route('/tumor', methods=['POST'])
 def predict_tumour_type():
     # Assigning the voting for tumor detection
@@ -400,17 +395,35 @@ def predict_alzheimer():
                            score=score_array)
 
 
+disease_status = {"Tumour": "Not Detected", "Tumour Type": "Not Detected", "Alzheimer": "Not Detected",
+                  "Stroke": "Not Detected", "Edge": "Not Detected"}
+
+disease_score = {"Tumour": 0.00, "Tumour Type": 0.00, "Alzheimer": 0.00, "Stroke": 0.00, "Edge": 0.00}
+
+required_image = None
+
+
 @app.route('/generateReport', methods=['POST'])
 def generateReport():
+    global disease_status, disease_score
     disease_status = {"Tumour": "Not Detected", "Tumour Type": "Not Detected", "Alzheimer": "Not Detected",
                       "Stroke": "Not Detected", "Edge": "Not Detected"}
-    disease_score = {"Tumour": 0, "Tumour Type": 0, "Alzheimer": 0, "Stroke": 0, "Edge": 0}
+
+    disease_score = {"Tumour": 0.00, "Tumour Type": 0.00, "Alzheimer": 0.00, "Stroke": 0.00, "Edge": 0.00}
+
+    label_mapping_side = {0: 'Axial', 1: 'Coronal', 3: 'Sagittal'}
     label_mapping_detector = {0: "Tumor", 1: "Normal"}
     label_mapping_classification = {0: 'Glioma', 1: 'Meningioma', 3: 'Pituitary', 2: 'NoTumor'}
     label_mapping_alzheimer = {'VeryMildDemented': 0, 'NonDemented': 1, 'ModerateDemented': 2, 'MildDemented': 3}
 
     imagefile = request.files['imagefile']
     image_path = "./static/PredictingAlzheimerImages/" + imagefile.filename
+
+    global required_image
+    required_image = image_path
+
+    print(required_image)
+
     print(image_path)
     imagefile.save(image_path)
     image = load_img(image_path, target_size=(256, 256))
@@ -435,6 +448,14 @@ def generateReport():
     all_disease_prediction = np.argmax(all_disease_vgg_19_probability)
 
     print(all_disease_prediction)
+
+    side_prediction = model_side_detection.predict(gamma_image_expand)
+    side_prediction_score = side_prediction[0][np.argmax(side_prediction)]
+    side_class = np.argmax(side_prediction)
+    side_name = label_mapping_side[side_class]
+
+    disease_status["Edge"] = side_name
+    disease_score["Edge"] = "{:.2f}".format(side_prediction_score)
 
     if all_disease_prediction == 0:
         detector_vgg_16_probability = tumor_vgg_16.predict(gamma_image_expand)[0]
@@ -465,8 +486,6 @@ def generateReport():
             disease_status["Tumour Type"] = predicted_class
             disease_score["Tumour Type"] = "{:.2f}".format(detector_score)
 
-            generate_pdf()
-
             return render_template('ReportGenerator.html', image_path=image_path, score=disease_score,
                                    predicted_class=disease_status)
 
@@ -476,8 +495,6 @@ def generateReport():
 
             disease_status["Tumour Type"] = label_mapping_detector[detector_prediction]
             disease_score["Tumour Type"] = "{:.2f}".format(detector_score)
-
-            generate_pdf()
 
             return render_template('ReportGenerator.html', image_path=image_path, score=disease_score,
                                    predicted_class=disease_status)
@@ -513,8 +530,6 @@ def generateReport():
 
         print(predicted_class)
 
-        generate_pdf()
-
         return render_template('ReportGenerator.html', image_path=image_path, score=disease_score,
                                predicted_class=disease_status)
 
@@ -534,98 +549,17 @@ def generateReport():
 
         disease_score["Stroke"] = "{:.2f}".format(prediction_score)
 
-        generate_pdf()
-
         return render_template('ReportGenerator.html', image_path=image_path, score=disease_score,
                                predicted_class=disease_status)
 
     return render_template('ReportGenerator.html', image_path=image_path)
 
 
-# pip install requests
-
-# The authentication key (API Key).
-# Get your own by registering at https://app.pdf.co
-API_KEY = "lakshancooray23@gmail.com_bx1ri33VsBaJRE18N2Kavme02m3ARIY1Y1LW44dj4P3dv7rF83rJ22YhZFGg7qS0"
-
-# Base URL for PDF.co Web API requests
-BASE_URL = "https://api.pdf.co/v1"
-
-# HTML template
-file_read = open("templates/report.html", mode='r', encoding='utf-8')
-SampleHtml = file_read.read()
-file_read.close()
-
-# Destination PDF file name
-DestinationFile = ".\\result.pdf"
-
-
-def generate_pdf(args=None):
-    GeneratePDFFromHtml(SampleHtml, DestinationFile)
-
-
-def GeneratePDFFromHtml(SampleHtml, destinationFile):
-    """Converts HTML to PDF using PDF.co Web API"""
-
-    # Prepare requests params as JSON
-    # See documentation: https://apidocs.pdf.co/?#1-json-pdfconvertfromhtml
-    parameters = {}
-
-    # Input HTML code to be converted. Required.
-    parameters["html"] = SampleHtml
-
-    #  Name of resulting file
-    parameters["name"] = os.path.basename(destinationFile)
-
-    # Set to css style margins like 10 px or 5px 5px 5px 5px.
-    parameters["margins"] = "0 0 0 0"
-
-    # Can be Letter, A4, A5, A6 or custom size like 200x200
-    parameters["paperSize"] = "Letter"
-
-    # Set to Portrait or Landscape. Portrait by default.
-    parameters["orientation"] = "Portrait"
-
-    # true by default. Set to false to disable printing of background.
-    parameters["printBackground"] = "true"
-
-    # If large input document, process in async mode by passing true
-    parameters["async"] = "false"
-
-    # Set to HTML for header to be applied on every page at the header.
-    parameters["header"] = ""
-
-    # Set to HTML for footer to be applied on every page at the bottom.
-    parameters["footer"] = ""
-
-    # Prepare URL for 'HTML To PDF' API request
-    url = "{}/pdf/convert/from/html".format(
-        BASE_URL
-    )
-
-    # Execute request and get response as JSON
-
-    response = requests.post(url, data=parameters, headers={"x-api-key": API_KEY})
-    if (response.status_code == 200):
-        json = response.json()
-
-        if json["error"] == False:
-            #  Get URL of result file
-            resultFileUrl = json["url"]
-            # Download result file
-            r = requests.get(resultFileUrl, stream=True)
-            if (r.status_code == 200):
-                with open(destinationFile, 'wb') as file:
-                    for chunk in r:
-                        file.write(chunk)
-                print(f"Result file saved as \"{destinationFile}\" file.")
-            else:
-                print(f"Request error: {response.status_code} {response.reason}")
-        else:
-            # Show service reported error
-            print(json["message"])
-    else:
-        print(f"Request error: {response.status_code} {response.reason}")
+@app.route('/report')
+def report():
+    print(required_image)
+    return render_template('report.html', score=disease_score,
+                           predicted_class=disease_status, image_path=required_image)
 
 
 if __name__ == '__main__':
